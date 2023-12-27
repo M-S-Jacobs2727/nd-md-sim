c55848ae662c91f00ea911741fa3b7d1978b7a55
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-26 19:22:26 -0500

    add constructor for Run and change name

--- src/run.jl
+++ src/run.jl
@@ -1,9 +1,13 @@
-export run!, RunSettings
+export run!, Run
 
-struct RunSettings
-    timestep::float
-    steps::Integer
+struct Run
+    timestep <: float
+    steps <: Unsigned
+    function Run(timestep <: float, steps <: Unsigned)
+        (timestep <= 0) && throw(DomainError("Timestep must be positive."))
+        new(timestep, steps)
+    end
 end
 
-function run!(atoms::Atoms, run::RunSettings, neighbor::NeighborSettings)
+function run!(atoms::Atoms, run::Run, neighbor::NeighborSettings)
 end
