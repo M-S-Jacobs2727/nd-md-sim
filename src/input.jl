8bac8c9e3154dcee5ceefa9beb4997eef970cf95
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-26 19:17:43 -0500

    add input.jl

--- src/input.jl
+++ src/input.jl
@@ -1,34 +1,55 @@
-export parseInput
+export parseFile
 
-function removeComment(line::AbstractString)
-    ind = findnext('#', line, 1)
-    ind === nothing && return strip(line)
-    return strip(line[1:ind])
+using YAML: YAML
+
+function loadfile(inputFile::AbstractString)::Dict{AbstractString, Any}
+    settingsDict::Dict{AbstractString, Any} = YAML.load_file(inputFile, dicttype = Dict{AbstractString, Any})
+
+    dimensions::Int = get(settingsDict, "dimensions", 3)
+    device::String = get(settingsDict, "device", "cpu")
+
+    boxdict = settingsDict["box"]
+    box = Box(
+        get(boxdict, "shape", "ortho"),
+        boxdict["bounds"],
+        get(boxdict, "pbc", "p"^length(boxdict["bounds"])),
+    )
+
+    neighbordict = settingsDict["neighbor"]
+    neigh = Neighbor(neighbordict["cutoff"], neighbordict["updateevery"])
+
+    return settingsDict
 end
 
-function readPairwiseParams!(paramArray::Array{Float64}, line::AbstractString, nAtomTypes::Int, nParams::Int)
-    tokens = split(line)
-    i = parse(Int, tokens[2])
-    j = parse(Int, tokens[3])
-    if i > nAtomTypes || j > nAtomTypes
-        throw(BoundsError("specified atom type too large: \"$line\""))
-    end
+# function removeComment(line::AbstractString)
+# 	ind = findnext('#', line, 1)
+# 	ind === nothing && return strip(line)
+# 	return strip(line[1:ind])
+# end
 
-    for k=1:nParams
-        p = parse(Float64, tokens[k+3])
-        paramArray[k, j, i] = p
-        i != j && (paramArray[k, j, i] = p)
-    end
-end
+# function readPairwiseParams!(paramArray::Array{Float64}, line::AbstractString, nAtomTypes::Int, nParams::Int)
+# 	tokens = split(line)
+# 	i = parse(Int, tokens[2])
+# 	j = parse(Int, tokens[3])
+# 	if i > nAtomTypes || j > nAtomTypes
+# 		throw(BoundsError("specified atom type too large: \"$line\""))
+# 	end
 
-function parseInput(inputfile::String, atoms::Atoms, subdomain::Subdomain)
-    open(inputfile, "r") do f
-        while !eof(f)
-            line = removeComment(readline(f))
-            line == "" && continue
-            if startswith(line, "pair_style")
-                
-            end
-        end
-    end
-end
+# 	for k ∈ 1:nParams
+# 		p = parse(Float64, tokens[k+3])
+# 		paramArray[k, j, i] = p
+# 		i != j && (paramArray[k, j, i] = p)
+# 	end
+# end
+
+# function parseInput(inputfile::String, atoms::Atoms, subdomain::Subdomain)
+# 	open(inputfile, "r") do f
+# 		while !eof(f)
+# 			line = removeComment(readline(f))
+# 			line == "" && continue
+# 			if startswith(line, "pair_style")
+
+# 			end
+# 		end
+# 	end
+# end
