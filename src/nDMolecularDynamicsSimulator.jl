573724aa65580ceac59de4bf1bd9af01050eecbd
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-29 14:50:19 -0500

    update main file

--- src/nDMolecularDynamicsSimulator.jl
+++ src/nDMolecularDynamicsSimulator.jl
@@ -1,10 +1,23 @@
 module nDMolecularDynamicsSimulator
 
+@everywhere using SharedArrays
 
 @everywhere include("atoms.jl")
 @everywhere include("box.jl")
 @everywhere include("neighbor.jl")
+@everywhere include("pbc.jl")
+
 @everywhere include("potentials/atomic.jl")
+@everywhere include("potentials/atomic/zero.jl")
+@everywhere include("potentials/atomic/ljcut.jl")
+
+@everywhere include("conditions.jl")
+@everywhere include("conditions/thermostats.jl")
+@everywhere include("conditions/thermostats/langevin.jl")
+
+@everywhere include("simulation.jl")
+@everywhere include("addatoms.jl")
+@everywhere include("velocity.jl")
 @everywhere include("verlet.jl")
 @everywhere include("input.jl")
 
