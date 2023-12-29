ddf39bc3d8026c1ca56c6e611fa53a4e76ed3cea
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-29 14:47:58 -0500

    add to neighbor.jl

--- src/neighbor.jl
+++ src/neighbor.jl
@@ -1,10 +1,17 @@
-export Neighbor, reneighbor!
+export Neighbor
 
 struct Neighbor
-    cutoff <: float
-    updateevery <: Integer
+    skin::AbstractFloat
+    updateevery::Unsigned
 end
 
-function reneighbor!(atoms::Atoms)
+function needs_reneighbor(atoms::Atoms, maxdist::AbstractFloat)::Bool
+end
 
+function reneighbor!(atoms::Atoms, maxdist::AbstractFloat)
+    # for each atom in writeview
+    #   for each atom in atom's neighborset
+    #       if distance > maxdist: remove from neighborset
+    #   for each atom in localview and ghostview
+    #       if not in neighborset and distance <= maxdist: add to neighborset
 end
