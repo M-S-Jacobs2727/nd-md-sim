13da54d44c6a92a579c8de3b42a1023c8079ffa5
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-29 14:43:31 -0500

    complete box.jl

--- src/box.jl
+++ src/box.jl
@@ -1,4 +1,4 @@
-export Box, BoxShape, BoundaryCondition
+export Box, BoxShape, BoundaryCondition, pbc!
 
 @enum BoxShape begin
     ortho
@@ -12,11 +12,11 @@ end
     shrink
 end
 
-struct Box{T <: AbstractFloat}
+struct Box{T1 <: AbstractFloat}
     shape::BoxShape
-    bounds::Matrix{T}
+    bounds::Matrix{T1}
     pbc::Matrix{BoundaryCondition}
-    function Box{T <: AbstractFloat}(shape::BoxShape, bounds::Matrix{T}, pbc::Matrix{BoundaryCondition})
+    function Box(shape::BoxShape, bounds::Matrix{T}, pbc::Matrix{BoundaryCondition}) where T <: AbstractFloat
         any(bounds[1, :] .>= bounds[2, :]) && throw(
             ArgumentError("Box bounds must be an nx2 matrix with bounds[1, :] < bounds[2, :]'."),
         )
@@ -24,38 +24,38 @@ struct Box{T <: AbstractFloat}
         (size(bounds, 1) == 2) || throw(ArgumentError("First dimension of bounds and pbc must be 2."))
         for col ∈ eachcol(pbc)
             # If exactly one of the pbcs in a given dimension is periodic, throw
-            ((col[1] == BoundaryCondition.periodic) ⊻ (col[2] == BoundaryCondition.periodic)) && throw(
+            ((col[1] == periodic) ⊻ (col[2] == periodic)) && throw(
                 ArgumentError("Box dimensions with periodic boundaries must be periodic on both sides."),
             )
         end
 
-        new(shape, bounds, pbc)
+        new{T}(shape, bounds, pbc)
     end
 end
 
-function Box{T <: AbstractFloat}(shape::AbstractString, bounds::Vector{T}, pbc::AbstractString)
-    if shape == "ortho"
-        _shape = BoxShape.ortho
-    elseif shape == "triclinic"
-        _shape = BoxShape.triclinic
-    else
-        throw(ArgumentError("Unknown box shape; must be either 'ortho' or 'triclinic'."))
-    end
+# function Box{T <: AbstractFloat}(shape::AbstractString, bounds::Vector{T}, pbc::AbstractString)
+#     if shape == "ortho"
+#         _shape = BoxShape.ortho
+#     elseif shape == "triclinic"
+#         _shape = BoxShape.triclinic
+#     else
+#         throw(ArgumentError("Unknown box shape; must be either 'ortho' or 'triclinic'."))
+#     end
 
-    _bounds = reshape(bounds, 2, :)
+#     _bounds = reshape(bounds, 2, :)
 
-    _pbc = fill(BoundaryCondition.periodic, 2, dim)
-    for i ∈ 1:2dim
-        if pbc[i] == 'f'
-            _pbc[i] = BoundaryCondition.fixed
-        elseif pbc[i] == 'm'
-            _pbc[i] = BoundaryCondition.minimum
-        elseif pbc[i] == 's'
-            _pbc[i] = BoundaryCondition.shrink
-        elseif pbc[i] != 'p'
-            throw(ArgumentError("Box pbc must be defined with the characters p, m, f, or s."))
-        end
-    end
+#     _pbc = fill(BoundaryCondition.periodic, 2, dim)
+#     for i ∈ 1:2dim
+#         if pbc[i] == 'f'
+#             _pbc[i] = BoundaryCondition.fixed
+#         elseif pbc[i] == 'm'
+#             _pbc[i] = BoundaryCondition.minimum
+#         elseif pbc[i] == 's'
+#             _pbc[i] = BoundaryCondition.shrink
+#         elseif pbc[i] != 'p'
+#             throw(ArgumentError("Box pbc must be defined with the characters p, m, f, or s."))
+#         end
+#     end
 
-    Box{T}(_shape, _bounds, _pbc)
-end
+#     Box{T}(_shape, _bounds, _pbc)
+# end
